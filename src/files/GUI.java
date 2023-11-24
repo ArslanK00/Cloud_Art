7545de2e9b2e695b8a394a3a51b62191efdd36de
rgarcia-101 <ruben@garcia100.com>
2023-11-24 05:27:25 -0600

    New panel layouts temp

--- src/files/GUI.java
+++ src/files/GUI.java
@@ -1,18 +1,24 @@
 package files;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.util.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
+/**
+ * GUI 
+ * 
+ * @author Rgarcia, ArslanK00
+ */
 public class GUI 
 {
     private int Gheight;
     private int Gwidth;
+
+    private JPanel ColorsPanel;
+    private JFrame frame;
+    private JPanel rightScreen;
+    private Button b;
 
     public GUI()
     {
@@ -30,48 +36,42 @@ public class GUI
 
     private void create()
     {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setSize(1249, 750);
         frame.setVisible(true);
         frame.setTitle("Fanbaslan");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(new Color(0x705549));
         frame.setMinimumSize(new Dimension(1250, 750));
+        frame.setLayout(new BorderLayout(50,50));
         
-        JPanel ColorsLeft = new JPanel(new GridLayout(5, 6));
         JPanel BottomFrame = new JPanel();
         JPanel TopRight = new JPanel();
         JPanel Drawing = new JPanel(new GridLayout(Gheight, Gwidth));
+
+
+        rightScreen = new JPanel(new FlowLayout());
+        rightScreen.setBackground(new Color(12345));
+        rightScreen.setPreferredSize(new Dimension(300, 1000));
         
         JPanel empty = new JPanel();
         empty.setVisible(false);
 
-        ColorsLeft.setBackground(new Color(0x46383c));
-        ColorsLeft.setBounds(0, 0, 200, 500);
+        
         
         Color[] c = {Color.red, Color.BLACK, Color.BLUE, Color.CYAN, Color.DARK_GRAY, Color.GRAY, Color.GREEN, Color.LIGHT_GRAY, Color.MAGENTA};
-        Button b;
-        for(int i = 1; i <= 6; i++)
-        { 
-            for(int j = 1; j <= 5; j++)
-            {
-                if( i * j < c.length)
-                {
-                    b = new Button(0, 0, 40, 100, c[i*j]);
-                }
-                else
-                { 
-                    b = new Button(0, 0, 40, 100, Color.white);
-                }
-                String temp = String.valueOf(b.getColor().getRed());
-                temp += " " + b.getColor().getGreen();
-                temp += " " + b.getColor().getBlue();
-                b.setActionCommand(temp);
-                ColorsLeft.add(b);
-            }
-        }
+        ColorsPanel = new JPanel(new GridLayout(3, c.length/2));
+        ColorsPanel.setBackground(new Color(0x46383c));
+        // ColorsPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 200, 100));
+        // ColorsPanel.setBounds(frame.getWidth()-200, 100, 200, 400);
+        ColorsPanel.setPreferredSize(new Dimension(300, 100));
+
         
-        frame.add(ColorsLeft);
+
+        makeCells(c);
+        
+        frame.add(rightScreen, BorderLayout.EAST);
+        rightScreen.add(ColorsPanel);
         
         BottomFrame.setBackground(Color.DARK_GRAY);
         BottomFrame.setBounds(200, 500, 900, 200);
@@ -84,5 +84,20 @@ public class GUI
 
         frame.add(empty);
         frame.setSize(1250, 750);
+    }
+
+    private void makeCells(Color[] colors) {
+        for (Color color: colors) {
+            b = new Button(0, 0, 40, 100, color);
+            ColorsPanel.add(b);
+
+            String temp = String.valueOf(b.getColor().getRed());
+            temp += " " + b.getColor().getGreen();
+            temp += " " + b.getColor().getBlue();
+            b.setActionCommand(temp);
+            ColorsPanel.add(b);
+            ColorsPanel.revalidate();
+            ColorsPanel.repaint();
+        }
     }
 }
